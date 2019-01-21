cce9f12f8ddab8be27d8fa5a9227f40352d0a26c
Mariusz Kohut <mariusz.kohut@gmail.com>
2019-01-21 06:40:45 +0100

    initial commit
    Initial commit

---
+++ src/main/java/com/capsilon/budgetapp/BudgetAppApplication.java
@@ -0,0 +1,14 @@
+package com.capsilon.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppApplication.class, args);
+    }
+
+}
+
